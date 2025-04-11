6db206744d0a92014126ac77acd450f0da158e5d
LiuLei-jm <14699396@qq.com>
2025-04-11 15:19:54 +0800

    添加项目文件。

---
+++ src/SimpleTrader.EntityFramework/SimpleTraderDbContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using SimpleTrader.Domain.Models;
+
+namespace SimpleTrader.EntityFramework
+{
+    public class SimpleTraderDbContext : DbContext
+    {
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<AssertTransaction> AssertTransactions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\MSSQLLocalDB;Database=SimpleTrader;Trusted_Connection=True"
+            );
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AssertTransaction>().OwnsOne(a => a.Stock);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
