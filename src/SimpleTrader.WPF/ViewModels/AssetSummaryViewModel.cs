2945e00a737c5f836f4f7a8be0de38b1ac11a37f
LiuLei-jm <14699396@qq.com>
2025-04-22 15:05:31 +0800

    使用 GridView 的 ListView 和完成主页视图

--- src/SimpleTrader.WPF/ViewModels/AssetSummaryViewModel.cs
+++ src/SimpleTrader.WPF/ViewModels/AssetSummaryViewModel.cs
@@ -6,15 +6,16 @@ namespace SimpleTrader.WPF.ViewModels
     public class AssetSummaryViewModel : ViewModelBase
     {
         private readonly AssetStore _assetStore;
-        private readonly ObservableCollection<AssetViewModel> _assets;
+        private readonly ObservableCollection<AssetViewModel> _topAssets;
         public double AccountBalance => _assetStore.AccountBalance;
-        public IEnumerable<AssetViewModel> Assets => _assets;
+        public IEnumerable<AssetViewModel> TopAssets => _topAssets;
 
         public AssetSummaryViewModel(AssetStore assetStore)
         {
             _assetStore = assetStore;
-            _assets = new ObservableCollection<AssetViewModel>();
+            _topAssets = new ObservableCollection<AssetViewModel>();
             _assetStore.StateChanged += OnAssetStoreChanged;
+            ResetAssets();
         }
 
         private void ResetAssets()
@@ -25,12 +26,13 @@ namespace SimpleTrader.WPF.ViewModels
                     g.Key,
                     g.Sum(a => a.IsPurchase ? a.Shares : -a.Shares)
                 ))
-                .Where(a => a.Shares > 0);
+                .Where(a => a.Shares > 0)
+                .OrderByDescending(a => a.Shares).Take(3);
 
-            _assets.Clear();
+            _topAssets.Clear();
             foreach (var viewModel in assetViewModels)
             {
-                _assets.Add(viewModel);
+                _topAssets.Add(viewModel);
             }
         }
 
